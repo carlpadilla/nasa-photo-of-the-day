bf8e1385fa0f38332a431ebdae80fb2a678b1456
carlpadilla <carlpadila@protonmail.com>
2019-07-30 14:48:02 -0400

    starting to build header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header.component';
 
 function App() {
   return (
     <div className='App'>
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun !
-      </p>
+      <Header />
     </div>
   );
 }
