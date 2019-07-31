8cc09a9346b8788e4b3536ed3ee813ad70dd8892
carlpadilla <carlpadilla@protonmail.com>
2019-07-31 08:52:02 -0400

    adding usestate and useeffect

--- src/components/PhotoCard/PhotoCard.component.jsx
+++ src/components/PhotoCard/PhotoCard.component.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 export default function PhotoCard({ limit }) {
+  const [photos, setPhoto] = useState([]);
+
+  useEffect(() => {
+    Axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY').then(
+      res => {
+        console.log('RESPONSE:', res.data.url);
+      }
+    );
+  });
+
   return (
     <div className='photo'>
-      <div>
-        <img
-          src='https://apod.nasa.gov/apod/image/1907/ngc3576_campbell_960.jpg'
-          alt='NASA Photo of the day'
-        />
-      </div>
+      {photos.map(photoUrl => {
+        return <img url={photos.data.url} />;
+      })}
     </div>
   );
 }
+
+// const images = [
+//     'https://apod.nasa.gov/apod/image/1907/ngc3576_campbell_960.jpg'
+//   ];
+
+// <div>
+// <img
+//   src='https://apod.nasa.gov/apod/image/1907/ngc3576_campbell_960.jpg'
+//   alt='NASA Photo of the day'
+// />
+// </div>
